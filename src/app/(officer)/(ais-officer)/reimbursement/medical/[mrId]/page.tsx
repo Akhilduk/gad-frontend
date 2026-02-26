86bae16ed95c3bdf99c92ae1cbcf8aef1f3f5425
Akhilduk <akhil.ss@duk.ac.in>
2026-02-26 15:29:48 +0530

    Broaden MR static params to include bracket-form path variants

--- src/app/(officer)/(ais-officer)/reimbursement/medical/[mrId]/page.tsx
+++ src/app/(officer)/(ais-officer)/reimbursement/medical/[mrId]/page.tsx
@@ -1,5 +1,11 @@
 import MedicalCaseWorkspaceClient from './medical-case-workspace-client';
 
+const staticMrIds = ['mr-1', 'mr-2', 'mr-3', 'mr-4', 'mr-5', 'mr-6', 'mr-7', 'mr-8', '[mrId]', '%5BmrId%5D'];
+
+export function generateStaticParams() {
+  return staticMrIds.map((mrId) => ({ mrId }));
+}
+
 export default async function MedicalCaseWorkspacePage({ params }: { params: Promise<{ mrId: string }> }) {
   const { mrId } = await params;
   return <MedicalCaseWorkspaceClient mrId={mrId} />;
